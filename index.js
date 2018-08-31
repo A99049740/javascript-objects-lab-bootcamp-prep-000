61658e1a0d326bf3b51b1df7cc5872d64bfb0198
jill zhang <talktojillzhang@gmail.com>
2018-08-31 20:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = new Object({"ob1":"first","ob2":"second"})
 function updateObjectWithKeyAndValue(object, key, value){
   let newob = new Object(object)
   newob[key]=value
+  console.log(newob)
   return newob
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -11,9 +12,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   let newob = new Object(object)
   delete newob[key]
+  console.log(newob)
   return newob
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
 }
+updateObjectWithKeyAndValue(recipes,"ob1","third")
